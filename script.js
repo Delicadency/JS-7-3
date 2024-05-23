d75d6972cb6915966bd2859a27a496880fea1f00
Agata Kamińska <164206709+Delicadency@users.noreply.github.com>
2024-05-23 18:35:25 +0000

    deleted comment, corrected fn

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Stwórz funkcję getInputValue, która będzie zwracała zawartość atrybutu value, gdy ten nie jest pusty, lub null gdy jest pusty. Funkcja w parametrze otrzyma dowolny element <input> i nie powinna go modyfikować, powinna jedynie zwrócić (return) zawartość atrybutu value lub null.
-
-W tym zadaniu musisz wykorzystać już wybrany dla Ciebie element (węzeł DOM (DOM Node)), który funkcja ma otrzymać przekazany, jako parametr w inpucie. Nie ma wobec tego potrzeby robić samemu document.querySelector("#test-input"), wystarczy jedynie (node) => -- i tutaj użycie node (pod którym kryje się <input>). */
-
-function getInputValue(value) {}
+function getInputValue(node) {
+  node.nodeValue === "" ? null : node.nodeValue;
+}
